6ab521429a5462d1139d9d69a5e24d62084fcc5e
Hiram Epifanio <hiramepifanio1@gmail.com>
2025-09-21 21:40:09 -0300

    Rename ViewMenu to ProjectsViewMenu pt. 2

--- src/pages/GeneralProjectsPage/GeneralProjectsPage.jsx
+++ src/pages/GeneralProjectsPage/GeneralProjectsPage.jsx
@@ -1,13 +1,13 @@
 import styles from './GeneralProjectsPage.module.css'
 import PageTitle from '../../components/PageTitle/PageTitle'
-import ViewMenu from '../../components/ProjectsViewMenu/ProjectsViewMenu'
+import ProjectsViewMenu from '../../components/ProjectsViewMenu/ProjectsViewMenu'
 
 export default function GeneralProjectsPage() {
   return (
     <>
       <PageTitle>General Projects</PageTitle>
       <section>
-        <ViewMenu />
+        <ProjectsViewMenu />
       </section>
     </>
   )
