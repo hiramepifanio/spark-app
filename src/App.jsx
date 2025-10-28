3bd092d5e94c1f46baf74daa65db581b03808aa6
Hiram Epifanio <hiramepifanio1@gmail.com>
2025-10-28 09:15:52 -0300

    Refactor Login page to use MUI components and Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { Routes, Route, Navigate } from "react-router-dom"
 import Layout from './components/Layout'
 import DashboardPage from './pages/DashboardPage/DashboardPage'
 import GeneralProjectsPage from './pages/GeneralProjectsPage/GeneralProjectsPage'
-import InternalProjectsPage from './pages/InternalProjectsPage/InternalProjectsPage'
-import ExternalProjectsPage from './pages/ExternalProjectsPage/ExternalProjectsPage'
-import StartupProjectsPage from './pages/StartupProjectsPage/StartupProjectsPage'
 import MeetingsPage from './pages/MeetingsPage/MeetingsPage'
 import SettingsPage from './pages/SettingsPage/SettingsPage'
 import SignupPage from './pages/SignupPage/SignupPage'
@@ -28,12 +25,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path='/' element={<Navigate to='/dashboard' replace />} />
               <Route path='/dashboard' element={<DashboardPage />} />
-              <Route path='/projects'>
-                <Route path='general' element={<GeneralProjectsPage />} />
-                <Route path='internal' element={<InternalProjectsPage />} />
-                <Route path='external' element={<ExternalProjectsPage />} />
-                <Route path='startups' element={<StartupProjectsPage />} />
-              </Route>
+              <Route path='/projects' element={<GeneralProjectsPage />} />
               <Route path='/meetings' element={<MeetingsPage />} />
               <Route path='/settings' element={<SettingsPage />} />
             </Route>
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,105 @@
-import LoginForm from '../../components/LoginForm/LoginForm'
-import LoginAndSignupPageLayout from '../../components/LoginAndSignupPageLayout/LoginAndSignupPageLayout'
-import styles from './LoginPage.module.css'
+import {
+  Box,
+  Container,
+  Paper,
+  TextField,
+  Typography,
+  Button,
+  Link,
+  Stack,
+} from "@mui/material";
+import { useContext, useState } from "react";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export default function LoginPage() {
+  const { authDispatch } = useContext(AuthContext)
+  const [errors, setErrors] = useState({})
+  const navigate = useNavigate()
+  
+  async function handleSubmit(event) {
+    event.preventDefault()
+    
+    const formData = new FormData(event.target)
+    const { email, password } = Object.fromEntries(formData.entries())
+    
+    let res = null
+    let resData = null
+    try {
+      res = await fetch('http://localhost:8000/api/token/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password })
+      })
+
+      resData = await res.json()
+    } catch (error) {
+      addErrorNotification(error.message)
+      return
+    }
+
+    // if (res.status === 400) {
+    //   setErrors(resData)
+    //   return
+    // }
+
+    // if (res.status === 401) {
+    //   addErrorNotification('Usuário ou senha incorretos')
+    //   return
+    // }
+
+    authDispatch({
+      type: 'LOGIN',
+      payload: resData
+    })
+    
+    navigate('/dashboard')
+  }
+
   return (
-    <LoginAndSignupPageLayout>
-      <LoginForm />
-    </LoginAndSignupPageLayout>
-  )
-}
+    <Container className="!max-w-xl flex flex-col h-screen">
+      <Box sx={{ flexGrow: 1 }} />
+      <Paper elevation={10} className="!p-4">
+        <Typography component="h1" variant="h5" className="!mb-2">
+          Entrar
+        </Typography>
+        <Typography component="p" variant="p" className="!mb-8">
+          Gerencie seus projetos de inovação de forma intuitiva.
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit}>
+          <Stack direction={'column'} gap={2}>
+            <TextField
+              id="email"
+              name="email"
+              label='Email'
+              placeholder="Digite seu email"
+              type="email"
+              required
+              autoFocus
+            />
+            <TextField
+              id="password"
+              name="password"
+              label='Senha'
+              placeholder="Digite sua senha"
+              required
+              type="password"
+            />
+          <Button type="submit" variant="contained">
+            Entrar
+          </Button>
+          </Stack>
+        </Box>
+        <Stack direction={'row'} justifyContent="space-between" className="!mt-4" >
+          <Link component={RouterLink} to="/forgot">
+            Esqueceu a senha?
+          </Link>
+          <Link component={RouterLink} to="/signup">
+            Cadastrar
+          </Link>
+        </Stack>
+      </Paper>
+      <Box sx={{ flexGrow: 1 }} />
+    </Container>
+  );
+};
