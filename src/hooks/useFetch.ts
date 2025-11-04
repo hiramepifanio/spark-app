1e262a062ab8e2542b96fd8251be9258047b565b
Hiram Epifanio <hiramepifanio1@gmail.com>
2025-11-04 12:22:54 -0300

    Refactor useFetch into ts

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,41 @@
+import { Dispatch, SetStateAction, useEffect, useState } from "react"
+import { useAPI } from "./useAPI"
+
+interface UseFetchResult<T> {
+    isLoading: boolean
+    fetchedData: T | null
+    setFetchedData: Dispatch<SetStateAction<T | null>>
+    errors: unknown | null
+}
+
+export function useFetch<T>(endpoint: string): UseFetchResult<T> {
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [errors, setErrors] = useState<unknown | null>(null)
+  const [fetchedData, setFetchedData] = useState<T | null>(null)
+  const { get } = useAPI()
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true)
+
+      const { status, isOk, data, errors } = await get<T>(endpoint)
+      
+      if(isOk) {
+        setFetchedData(data)
+      } else {
+        setErrors(errors)
+      }
+      
+      setIsLoading(false)
+    }
+
+    fetchData()
+  }, [endpoint])
+
+  return {
+    isLoading,
+    fetchedData,
+    setFetchedData,
+    errors
+  }
+}
